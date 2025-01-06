34f2456a8a0443292688111baaf7e9ddf8dc5220
Robert Schulze <robert@dotless.de>
2025-01-06 11:30:55 +0100

    Replace magic getter/setter for phlex options with explicit set_ methods

--- spec/support/test_app_helper.rb
+++ spec/support/test_app_helper.rb
@@ -85,29 +85,31 @@ module TestAppHelper
           r.is do
             r.get do
               if (title = r.params["title"])
-                phlex_layout_opts title: title
+                set_phlex_layout_opts title: title
               end
               phlex FooView.new, content_type: "text/html"
             end
           end
 
           r.get "nil" do
-            phlex_layout nil
+            set_phlex_layout nil
             phlex FooView.new
           end
 
           r.get "false" do
-            phlex_layout false
+            set_phlex_layout false
             phlex FooView.new
           end
 
           r.get "phlex_layout_override" do
-            phlex_layout AlternativeLayout
-            phlex_layout_opts title: "phlex_layout_override"
+            set_phlex_layout AlternativeLayout
+            set_phlex_layout_opts title: "phlex_layout_override"
             phlex FooView.new("content")
           end
 
           r.on "merge_opts_route_override" do
+            # Directly mutating the plugin config is not recommended.
+            # Use `set_phlex_layout` and `set_phlex_layout_opts` instead.
             opts[:phlex][:layout] = AlternativeLayout
             opts[:phlex][:layout_opts] = {title: "route-title"}
 
@@ -118,11 +120,37 @@ module TestAppHelper
               phlex FooView.new, content_type: "text/html"
             end
           end
+
+          r.on "mutate_opts" do
+            r.get "unsafe" do
+              if (title = r.params["title"])
+                # DON'T do this. This will mutate the plugin config and carry over to the next request.
+                phlex_layout_opts[:title].prepend title, " - "
+              end
+              phlex FooView.new, content_type: "text/html"
+            end
+
+            r.get "safer" do
+              if (title = r.params["title"])
+                # Mutating the shallow copy of layout options
+                phlex_layout_opts[:title] = phlex_layout_opts[:title].dup.prepend title, " - "
+              end
+              phlex FooView.new, content_type: "text/html"
+            end
+
+            r.get "safe" do
+              if (title = r.params["title"])
+                # Setting a new layout options hash
+                set_phlex_layout_opts phlex_layout_opts.merge(title: "#{title} - #{phlex_layout_opts[:title]}")
+              end
+              phlex FooView.new, content_type: "text/html"
+            end
+          end
         end
 
         r.on "layout_handler" do
-          phlex_layout AlternativeLayout
-          phlex_layout_handler ->(layout, _opts, obj) {
+          set_phlex_layout AlternativeLayout
+          set_phlex_layout_handler ->(layout, _opts, obj) {
             layout.new(obj, title: "phlex_layout_handler override")
           }
 
@@ -131,12 +159,12 @@ module TestAppHelper
           end
 
           r.get "reset_via_nil" do
-            phlex_layout_handler nil
+            set_phlex_layout_handler nil
             phlex FooView.new
           end
 
           r.get "reset_via_default" do
-            phlex_layout_handler :default
+            set_phlex_layout_handler :default
             phlex FooView.new
           end
         end
